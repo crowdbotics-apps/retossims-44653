e515d3a8f596025a670c0d713cdf476795ce6056
Crowdbotics <team@crowdbotics.com>
2023-11-17 22:09:59 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "retossims-44653",
+  url: "https://retossims-44653.botics.co",
+  api: "https://retossims-44653.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
